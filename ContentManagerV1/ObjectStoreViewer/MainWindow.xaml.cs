a34a424ec1f81ce86de29cf0be29833a0c69d2ce
Manoj Patel <sw@mpvisual.com>
2013-09-24 05:02:27 -0700

    add project to view directory structure etc
    still prototype ugly code, not finished

--- ContentManagerV1/ObjectStoreViewer/MainWindow.xaml.cs
+++ ContentManagerV1/ObjectStoreViewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MpvUtilities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+
 
 namespace ObjectStoreViewer
 {
@@ -20,6 +24,7 @@ namespace ObjectStoreViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -27,8 +32,50 @@ namespace ObjectStoreViewer
 
         private void OnChooseRootDirButtonClick(object sender, RoutedEventArgs e)
         {
+            string workingDir;
+            string dirname = FilePickerUtility.PickDirectory();
+            if ((dirname != null) && (dirname != String.Empty))
+            {
+                // for now disable the button. In real version allow user to change the root directory
+                ChooseRootDirButton.Visibility = System.Windows.Visibility.Collapsed;
+                directoryTreeView.Visibility = System.Windows.Visibility.Visible;
+
+                workingDir = System.IO.Path.Combine(dirname, "working");
+                if (Directory.Exists(workingDir))
+                {
+                    ProcessWorkingDir(workingDir);
+                }
+            }
+
+        }
+
+        private void ProcessWorkingDir(string workingDir)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(workingDir);
+            foreach (FileInfo file in dirInfo.EnumerateFiles("*.xml", SearchOption.TopDirectoryOnly))
+            {
+                XDocument dirXml = XDocument.Load(file.FullName);
+                string dirPath = dirXml.Root.Attribute("path").Value.ToString();
+                TreeViewItem newItem = new TreeViewItem();
+                newItem.Header = dirPath;
+                directoryTreeView.Items.Add(newItem);
+                newItem.Selected += newItem_Selected;
 
 
+            }
+        }
+
+        void newItem_Selected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+
+            RecursivelyAddDirectoriesAndFiles(dirPath, item);
+
+        }
+
+        private void RecursivelyAddDirectoriesAndFiles(string dirPath, TreeViewItem newItem)
+        {
+            throw new NotImplementedException();
         }
     }
 }
